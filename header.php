7646d2a0081027db712fc19704fdedb8b2e7b65e
Sina Maghsoudi <132477568+sinas12@users.noreply.github.com>
2025-10-17 16:43:38 +0330

    Update header.php
    set header nav menu to static if there is no nav set in wordpress settings

--- header.php
+++ header.php
@@ -67,16 +67,29 @@ if (has_nav_menu($menu_location)) {
 			</button>
 		</div>
 		 <?php if (!empty($menu_items)): ?>
-		<div class="site-menu items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-			<ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-				<?php foreach ($menu_items as $item): ?>
-			<li>
-				<a href="<?php echo esc_url($item->url); ?>" class="block py-2 px-3 text-white bg-sky-500/75 hover:bg-sky-500/100 sm:bg-transparent sm:hover:bg-transparent transform transition my-2 sm:my-0 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page"> <?php echo esc_html($item->title); ?></a>
-			</li>
-			<?php endforeach; ?>
+			<div class="site-menu items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+				<ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+					<?php foreach ($menu_items as $item): ?>
+				<li>
+					<a href="<?php echo esc_url($item->url); ?>" class="block py-2 px-3 text-white bg-sky-500/75 hover:bg-sky-500/100 sm:bg-transparent sm:hover:bg-transparent transform transition my-2 sm:my-0 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page"> <?php echo esc_html($item->title); ?></a>
+				</li>
+				<?php endforeach; ?>
 
-			</ul>
-		</div>
+				</ul>
+			</div>
+		<?php else: ?>
+			<div class="site-menu items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+				<ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+
+				<li>
+					<a href="<?php echo esc_url($site_url); ?>" class="block py-2 px-3 text-white bg-sky-500/75 hover:bg-sky-500/100 sm:bg-transparent sm:hover:bg-transparent transform transition my-2 sm:my-0 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+				</li>
+				<li>
+					<a href="#" class="block py-2 px-3 text-white bg-sky-500/75 hover:bg-sky-500/100 sm:bg-transparent sm:hover:bg-transparent transform transition my-2 sm:my-0 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">About us</a>
+				</li>
+
+				</ul>
+			</div>
 		 <?php endif; ?>
 		</div>
 		</nav>
